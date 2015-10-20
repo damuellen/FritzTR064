6d0c5277fd3579574ccb5f6f4072fab2503dcf02
Daniel Müllenborn <damuellen@aol.com>
2015-10-20 21:58:38 +0200

    Bugs in search patched

---
+++ Fritz TR064/Extensions.swift
@@ -0,0 +1,13 @@
+//
+//  Extensions.swift
+//  Fritz TR064
+//
+//  Created by Daniel Müllenborn on 20/10/15.
+//  Copyright © 2015 Daniel Müllenborn. All rights reserved.
+//
+
+extension String {
+  var isEmpty: Bool  {
+    return self.characters.count == 0
+  }
+}
