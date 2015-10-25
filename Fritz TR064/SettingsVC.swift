e4c2932e4e19a52f3c138b063177746a42ec4f17
Daniel <damuellen@aol.com>
2015-10-25 15:12:04 +0100

    Settings view

---
+++ Fritz TR064/SettingsVC.swift
@@ -0,0 +1,22 @@
+//
+//  SettingsVC.swift
+//  Fritz TR064
+//
+//  Created by Daniel Müllenborn on 25/10/15.
+//  Copyright © 2015 Daniel Müllenborn. All rights reserved.
+//
+
+import UIKit
+
+class SettingsVC: UITableViewController {
+	
+	@IBOutlet weak var routerIP: UITextField!
+	@IBOutlet weak var routerPassword: UITextField!
+	@IBOutlet weak var vpnAdresse: UITextField!
+	@IBOutlet weak var vpnUser: UITextField!
+	@IBOutlet weak var vpnGroup: UITextField!
+	@IBOutlet weak var vpnPassword: UITextField!
+	@IBOutlet weak var sharedSecret: UITextField!
+	@IBOutlet var allTextFields: [UITextField]!
+	
+}
