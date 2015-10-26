836758223e71ed42c4d73f476714a4cf01c9bc55
Daniel Müllenborn <damuellen@aol.com>
2015-10-26 19:49:41 +0100

    changed from explicit to implicit unwrapping

--- Fritz TR064/FadePushSegue.swift
+++ Fritz TR064/FadePushSegue.swift
@@ -13,10 +13,12 @@ class FadePushSegue: UIStoryboardSegue {
 	override func perform() {
 		let source = sourceViewController.navigationController!
 		let destination = ((destinationViewController as! UINavigationController).topViewController as! XMLResponseViewController)
-		(destination.bgView.layer.sublayers![0] as! CAGradientLayer).colors =
-			((sourceViewController as! ActionArgumentsVC).bgView.layer.sublayers![0] as! CAGradientLayer).colors
+		(destination.bgView.layer.sublayers?.first as? CAGradientLayer)?.colors =
+      ((sourceViewController as! ActionArgumentsVC).bgView.layer.sublayers?.first as? CAGradientLayer)?.colors
 		let transition = CATransition()
-		transition.duration = 1.0
+		transition.duration = 1.5
+    transition.timingFunction = CAMediaTimingFunction(name:
+      kCAMediaTimingFunctionEaseIn)
 		transition.type = kCATransitionFade
 		source.navigationController?.view.layer.addAnimation(transition, forKey:kCATransition)
 		source.pushViewController(destination, animated: false)
